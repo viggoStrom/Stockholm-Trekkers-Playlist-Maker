95d132d125ffc4ed7eddf1da1eef176530963cc5
viggoStrom <viggostrom05@gmail.com>
2024-09-19 17:04:42 +0200

    ffmpeg solution

--- scripts/getMetaData.js
+++ scripts/getMetaData.js
@@ -2,18 +2,30 @@ const { ipcMain } = require("electron");
 const fs = require("fs");
 const path = require("node:path")
 const raiseError = require("./raiseError.js");
-
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static");
 
 const getMetaData = (filePath) => {
-    const file = fs.statSync(filePath);
-    console.log(file);
+    return new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(filePath, (error, metadata) => {
+            if (error) {
+                console.log("[ERROR] getMetaData: ", error);
+                reject(raiseError("getMetaData", error));
+            } else {
+                console.log("[INFO] getMetaData: ", metadata);
+                resolve(metadata);
+            }
+        });
+    });
 };
 
 const setUpHandlers = () => {
+    ffmpeg.setFfmpegPath(ffmpegPath);
+    console.log("[INFO] ffmpegPath: ", ffmpegPath);
+
     ipcMain.handle("get-meta-data", getMetaData);
 };
 
 module.exports = { setUpHandlers };
 
-console.log("[DEBUG] getMetaData.js loaded");
 getMetaData("assets/videos/pause_1_min_covid.mp4");
